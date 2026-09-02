9e366195c1be74f647b04886a86d5bb123812565
MJ <theub002@gmail.com>
2026-09-02 17:42:48 +0530

    change

--- backend/database.py
+++ backend/database.py
@@ -2,9 +2,9 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
-from dotenv import load_dotenv
+from dotenv import load_dotenv, find_dotenv
 
-load_dotenv()
+load_dotenv(find_dotenv(usecwd=True, fallback=True))
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
